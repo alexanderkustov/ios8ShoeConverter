27d414a9ee75bcf1dbd4707cd89f56a251b8846f
Alexander K <knuxus@gmail.com>
2014-09-10 17:14:03 +0100

    shoe converted done

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var menShoeSizeTextField: UITextField
+    @IBOutlet var convertedLabel: UILabel
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertWomenPressed(sender: UIButton) {
+        
+        
+        
+        
+        let shoeNumber = Double((menShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        
+        
+        convertedLabel.hidden = false
+        
+        convertedLabel.text = "\(shoeNumber + conversionConstant)" + " in European shoe size"
+        
+    }
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let shoeNumber = menShoeSizeTextField.text.toInt()!
+        
+        convertedLabel.hidden = false
+        
+        convertedLabel.text = "\(shoeNumber + 30)" + " in European shoe size"
+        
+    }
 
 }
 
